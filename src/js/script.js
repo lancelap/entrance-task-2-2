679ee3fe450da66acab53fae11a7b682ceab128a
Jonh Doe <lancelap@mail.ru>
2018-07-29 19:37:35 +0500

    add srcipt.js

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+const mainNav = document.querySelector(".main-nav");
+const navToggle = document.querySelector(".main-nav__toggle");
+
+navToggle.addEventListener("click", function(event) {
+  if (mainNav.classList.contains("main-nav--closed")) {
+    mainNav.classList.remove("main-nav--closed");
+    mainNav.classList.add("main-nav--opened");
+  } else {
+    mainNav.classList.add("main-nav--closed");
+    mainNav.classList.remove("main-nav--opened");
+  }
+
+});
